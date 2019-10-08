7de751c9c9a979323d00cdb2d23553a9c394228d
yudar1024 <rojer.chen@foxmail.com>
2019-10-08 18:16:14 +0800

    update

---
+++ src/main/java/com/roger/javainterview/java11/optionalexample/OptionalSample.java
@@ -0,0 +1,36 @@
+package com.roger.javainterview.java11.optionalexample;
+
+import java.util.Optional;
+
+public class OptionalSample {
+
+  private static void showOptionalUse() {
+    Optional<Integer> price1 = Optional.of(0);
+    Optional<Integer> price2 = Optional.empty();
+    Optional<Integer> price3 = Optional.ofNullable(null);
+    price3.ifPresentOrElse(p -> {
+      System.out.println(p);
+    }, () -> System.out.println("null value"));
+    price3.ifPresentOrElse(p -> {
+      processValue(p);
+    }, () -> System.out.println("null value"));
+    // 当null时 返回42
+    int p2 = price2.orElseGet(() -> 42);
+    int p3 = price3.orElse(33);
+    System.out.println("p2=" + p2);
+    System.out.println("p3=" + p3);
+    price3 = Optional.of(10);
+    price3.ifPresentOrElse(p -> {
+      System.out.println(p);
+    }, () -> System.out.println("null value"));
+    price3.ifPresentOrElse(p -> {
+      processValue(p);
+    }, () -> System.out.println("null value"));
+
+  }
+
+  private static void processValue(int value) {
+    System.out.println(value);
+  }
+
+}
